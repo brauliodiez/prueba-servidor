6954072ee219dfcc5cf13997f7d64d235f531e58
Braulio Diez Botella <braulio.diez@gmail.com>
2022-03-18 17:35:55 +0100

    actualizaciones

--- ficherob.js
+++ ficherob.js
@@ -1,3 +1,9 @@
 console.log("-------------");
 console.log("soy el segundo fichero");
 console.log("-------------");
+ console.log(`
+  Ala Celta a demostrar, por historia e tradición,
+  que o teu lema é nobre xogo, afouteza e corazón
+  Cando xogas con tesón, vibra toda a afección,
+  ala Celta berramos, ala Celta campión.
+ `);
